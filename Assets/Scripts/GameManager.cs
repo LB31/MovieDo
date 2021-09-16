41599509024b78855dc1748ed683bff4548b4a64
misc <milena.schwarz@hs-augsburg.de>
2021-09-16 18:05:23 +0200

    New Scale

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
     public GameObject CurrentContent;
+
+    public void ChangeScene(string scenName)
+    {
+        SceneManager.LoadScene(scenName, LoadSceneMode.Single);
+    }
 }
